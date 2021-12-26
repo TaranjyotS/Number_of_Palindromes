c721832d88faa709a5ae8e9b3c01714966563153
Taranjyot Singh <31364683+TaranjyotS@users.noreply.github.com>
2021-12-26 00:31:14 -0500

    Add files via upload
    The project proceeds by laying out 3 necessary functions:
    1. find_palindrome - Checks if the substring of a given string is palindrome or not.
    2. solution - Initializes loop which checks for the same character at position 1(start) in the string ahead, marks it as a substring, and checks if it is a palindrome or not. Then the start position is incremented by 1. After covering all the palindromic substrings, a list of palindromic substrings is created and sorted in reverse order by length. In the end, all palindromic substrings are printed along with their length and position of the starting character of the substring in the given string.
    3. test_is_palindrome - Checks if both the functions are working properly by running some unit tests on them. The solution() function is run through a series of different strings(characters, numbers, special characters) to check for palindromic substrings. The test case that fails the unit test is presented by "F" in the output terminal.

---
+++ palindrome.py
@@ -0,0 +1,66 @@
+import re
+
+# Function to check if the string is palindrome or not.
+def find_palindrome(s):
+    return s == s[::-1]
+
+def solution(s):
+    palindrome_list = []
+    no_of_palindromes = 0
+    for i in range(len(s)):
+        start = s[i]
+        same_char = s[i::]
+        # re.finditer yields match objects matching the regex pattern in a string.
+        for match in re.finditer(start, same_char):
+            if match.start()!=0:
+                sub_string = same_char[0:match.start()+1:]
+                
+                if find_palindrome(sub_string) == True:
+                    no_of_palindromes += 1
+                    if i<10:
+                        sub_string = sub_string+str(0)+str(i)
+                    else:
+                        sub_string = sub_string+str(i)
+                    
+                    # Append all the substring palindromes into a new list.
+                    palindrome_list.append(sub_string)
+                    # Sorting the new palindrome list by the length of the substring in reverse order.
+                    palindrome_list = sorted(palindrome_list, key=lambda palindrome_list: (-len(palindrome_list),palindrome_list))
+    
+    if len(palindrome_list) == 0:
+        print("No palindromes in the given string.")            
+    else:
+        print("Number of palindromes in the string are", no_of_palindromes)
+        # Printing palindromes with their length and position.
+        for i in palindrome_list:
+            print(i[:len(i)-2:],",", len(i[:len(i)-2:]),",",i[len(i)-2:len(i):])
+        print("\n")
+
+    return no_of_palindromes
+
+solution("ABCBAHELLOHOWRACECARAREYOUILOVEUEVOLIIAMAIDOINGGOOD")
+#solution("1234543223454531123211")
+#solution("!aabbccaaccbbaa!")
+#solution("@!#&@$%&@!@&!")
+#solution("a23432a43234a")
+#solution("adgbuyebjkao")
+
+import unittest
+
+class TestPalindromes(unittest.TestCase):
+
+    def test_is_palindrome(self):
+        # Testing find_palindrome function to see it gives right resuts.
+        assert find_palindrome('ARA') is True 
+        assert find_palindrome('ILOVEUEVOLI') is True
+
+        # Testing solution function to get if there is any palindrome present in the given string.
+        assert solution('ABCBAHELLOHOWRACECARAREYOUILOVEUEVOLIIAMAIDOINGGOOD') != 0
+        assert solution("1234543223454531123211") != 0
+        assert solution("!aabbccaaccbbaa!")  != 0
+        assert solution("@!#&@$%&@!@&!")  != 0
+        assert solution("a23432a43234a") != 0
+        assert solution('adgbuyebjkao') != 0  
+        
+if __name__ == '__main__':
+    unittest.main()
